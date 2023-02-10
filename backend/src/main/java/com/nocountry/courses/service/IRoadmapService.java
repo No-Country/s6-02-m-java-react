054f48823e13cc4fb114f6cbb0c7df7bc4d9efe8
davaloslm <davaloslm@hotmail.com.ar>
2023-02-10 13:04:48 -0300

    Create roadmap service interface and impl class

---
+++ backend/src/main/java/com/nocountry/courses/service/IRoadmapService.java
@@ -0,0 +1,7 @@
+package com.nocountry.courses.service;
+
+import com.nocountry.courses.dto.request.RoadmapRequestDto;
+import com.nocountry.courses.dto.response.RoadmapResponseDto;
+
+public interface IRoadmapService extends ICrudService<RoadmapResponseDto, RoadmapRequestDto> {
+}
