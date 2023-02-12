64199804104c6c6436cf810cf5552978a147b23e
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-02-12 17:18:08 -0300

    login page maded, desktop view looks great but mobile view still need some details

--- frontend/Pro-Grama/src/pro-grama/components/Sidebar.jsx
+++ frontend/Pro-Grama/src/pro-grama/components/Sidebar.jsx
@@ -33,7 +33,7 @@ const Sidebar = () => {
         className="fixed top-0 left-0 z-10 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
         aria-label="Sidebar"
       >
-        <div className="h-full px-2 py-[3rem] overflow-y-auto bg-gray-50 dark:bg-gray-800">
+        <div className="h-full px-2 py-[3rem] text-center overflow-y-auto bg-gray-50 dark:bg-gray-800">
           <Link to="/" className="flex items-center pl-2.5 mb-5">
             <img
               src="https://flowbite.com/docs/images/logo.svg"
