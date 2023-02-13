4239842282d1c5211dde84ea9958ac81efce2c02
luisnava6667 <luisnavarro19890603@gmail.com>
2023-02-13 08:43:55 -0300

    curso

--- frontend/Pro-Grama/src/pro-grama/routes/mainRoute.jsx
+++ frontend/Pro-Grama/src/pro-grama/routes/mainRoute.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+import { CursosPage } from "../pages/CursosPage";
 import { HomePage } from "../pages/HomePage";
 
 export const MainRoute = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/curso" element={<CursosPage />} />
 
       <Route path="/*" element={<Navigate to="/" />} />
     </Routes>
