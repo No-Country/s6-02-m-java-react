875e7226558b09a880947dd5f7e172f160a42622
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-02-14 11:09:31 -0300

    added home route to course page

--- frontend/Pro-Grama/src/pro-grama/components/curso/Description.jsx
+++ frontend/Pro-Grama/src/pro-grama/components/curso/Description.jsx
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { AiFillHeart,  AiOutlineHeart, AiOutlinePlus , AiOutlineClockCircle , AiOutlineUnorderedList } from "react-icons/ai";
+import {
+  AiFillHeart,
+  AiOutlineHeart,
+  AiOutlinePlus,
+  AiOutlineClockCircle,
+  AiOutlineUnorderedList,
+} from "react-icons/ai";
 import { GrPlay } from "react-icons/gr";
 import { Button } from "flowbite-react";
 import ReactPlayer from "react-player";
 import "./curso.css";
+import { Link } from "react-router-dom";
 
 const courseJsData = {
   title: "Javascript",
@@ -15,42 +22,53 @@ const courseJsData = {
 };
 
 export const Description = () => {
+  const [hearthPress, setHearthPress] = useState(true);
 
-  const [hearthPress, setHearthPress] = useState(true)
-
-  
-  const handleHearth = () =>{
-    setHearthPress(!hearthPress)
-  }
+  const handleHearth = () => {
+    setHearthPress(!hearthPress);
+  };
 
   return (
     <>
+    <div></div>
       <div className="flex items-center px-32 my-4">
         <div className="flex justify-between w-3/5 items-center">
           <h2 className="text-3xl">{`Curso de ${courseJsData.title}`}</h2>
-          <Button color="white" onClick={handleHearth} >
-            {hearthPress ?  <AiOutlineHeart className="w-8 h-8" /> : <AiFillHeart className="w-8 h-8" /> }
+          <Button color="white" onClick={handleHearth}>
+            {hearthPress ? (
+              <AiOutlineHeart className="w-8 h-8" />
+            ) : (
+              <AiFillHeart className="w-8 h-8" />
+            )}
           </Button>
         </div>
         <div className="flex justify-end w-2/5">
-          <Button className="h-8 bg-slate-300 text-black hover:bg-slate-300" >
+          <Button className="h-8 bg-slate-300 text-black hover:bg-slate-300">
             <GrPlay className="mr-2" />
             Comenzar curso
           </Button>
         </div>
       </div>
+
+
       <div className="flex px-32">
-        <div className="flex-col w-3/5">
+        <div className="flex-col">
           <ReactPlayer
-            url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
+            url="https://www.youtube.com/watch?v=rbuYtrNUxg4&ab_channel=Fazt"
             controls
             volume="0.5"
-            width={'100%'}
-            height={'140%'}
+            width={"53rem"}
+            height={"30rem"}
           />
           <div className="flex justify-between p-4">
-            <p className="py-4 flex items-center gap-2"> <AiOutlineUnorderedList/> {`${courseJsData.lessons} lecciones`}</p>
-            <p className="py-4 flex items-center gap-2"> <AiOutlineClockCircle/> {`${courseJsData.hours} horas`}</p>
+            <p className="py-4 flex items-center gap-2">
+              {" "}
+              <AiOutlineUnorderedList /> {`${courseJsData.lessons} lecciones`}
+            </p>
+            <p className="py-4 flex items-center gap-2">
+              {" "}
+              <AiOutlineClockCircle /> {`${courseJsData.hours} horas`}
+            </p>
             <p className="bg-slate-200 rounded-full py-2 px-4 self-center">
               {courseJsData.level}
             </p>
@@ -64,10 +82,14 @@ export const Description = () => {
           </Button>
         </div>
         <div>
+          <button>
+            <Link to="/">Hola</Link>
+          </button>
           <p>lista de videos</p>
         </div>
-        
       </div>
+
+      
     </>
   );
 };
