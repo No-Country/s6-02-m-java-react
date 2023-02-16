8cfbcb2bf3c24e68775ba6717bf9f25bdb47918a
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-02-16 11:58:00 -0300

    testing solutions on notes components

--- frontend/Pro-Grama/src/pro-grama/components/SidebarItems.jsx
+++ frontend/Pro-Grama/src/pro-grama/components/SidebarItems.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AiFillHome } from "react-icons/ai";
 import { RxDashboard } from "react-icons/rx";
 import { FaChalkboardTeacher } from "react-icons/fa";
-import {FiLayout} from "react-icons/fi"
+import { FiLayout } from "react-icons/fi";
 import { CgProfile } from "react-icons/cg";
 import { VscSignIn } from "react-icons/vsc";
 import { BiLogIn } from "react-icons/bi";
@@ -36,8 +36,10 @@ const SidebarItems = () => {
           to="/CourseDetail"
           className="flex items-center  p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
         >
-          <FaChalkboardTeacher  className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
-          <span className="flex-1 ml-3 whitespace-nowrap hidden">Leccion Actual</span>
+          <FaChalkboardTeacher className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+          <span className="flex-1 ml-3 whitespace-nowrap hidden">
+            Leccion Actual
+          </span>
         </Link>
       </li>
 
@@ -57,7 +59,9 @@ const SidebarItems = () => {
           className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
         >
           <VscSignIn className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
-          <span className="flex-1 ml-3 whitespace-nowrap hidden">Registrarse</span>
+          <span className="flex-1 ml-3 whitespace-nowrap hidden">
+            Registrarse
+          </span>
         </Link>
       </li>
 
@@ -67,7 +71,9 @@ const SidebarItems = () => {
           className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
         >
           <BiLogIn className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
-          <span className="flex-1 ml-3 whitespace-nowrap hidden">Iniciar Sesion</span>
+          <span className="flex-1 ml-3 whitespace-nowrap hidden">
+            Iniciar Sesion
+          </span>
         </Link>
       </li>
 
@@ -77,7 +83,7 @@ const SidebarItems = () => {
           className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
         >
           <AiFillHome className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
-          <span className="px-3 hidden">Inicio</span>
+          <span className="px-3 hidden">Notas</span>
         </Link>
       </li>
     </ul>
