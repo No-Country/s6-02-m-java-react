b540079ac5f9d89eea199be676522c1ee71ec2ed
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-02-18 16:37:54 -0300

    landing page started with basic design

--- frontend/Pro-Grama/src/pro-grama/components/SidebarItems.jsx
+++ frontend/Pro-Grama/src/pro-grama/components/SidebarItems.jsx
@@ -10,7 +10,7 @@ import { Link } from "react-router-dom";
 
 const SidebarItems = () => {
   return (
-    <ul className="space-y-2 py-5 px-2 text-start navbar">
+    <ul className="space-y-3 py-5 px-3 text-start navbar">
       <li>
         <Link
           to="/"
@@ -82,9 +82,21 @@ const SidebarItems = () => {
           to="/CourseLessons"
           className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
         >
-          <AiFillHome className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+          <RxDashboard className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
           <span className="px-3 hidden">Notas</span>
         </Link>
+
+        <li>
+          <Link
+            to="/Home"
+            className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+          >
+            <BiLogIn className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+            <span className="flex-1 ml-3 whitespace-nowrap hidden">
+              LandingPage
+            </span>
+          </Link>
+        </li>
       </li>
     </ul>
   );
