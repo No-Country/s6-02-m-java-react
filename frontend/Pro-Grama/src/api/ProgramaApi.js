8248d204b77411b8992356d06c7d90e0335e4e66
FranciscoSantos01 <franciscosantosaraoz2000@gmail.com>
2023-02-23 09:48:00 -0300

    configuracion login y registro

---
+++ frontend/Pro-Grama/src/api/ProgramaApi.js
@@ -0,0 +1,22 @@
+// import axios from 'axios'
+// import {getEnvVariables} from '../helpers/getEnvVariables'
+
+// const {VITE_API_URL }=getEnvVariables
+// const programaApi = axios.create({
+//     baseURL:VITE_API_URL,
+// })
+
+
+// programaApi.interceptors.request.use(config=>{
+//     config.headers = {
+//         ...config.headers,
+//         'Authorization': `Bearer ${localStorage.getItem('token')}`
+//     }
+//     return config;
+// })
+
+
+
+
+
+export default programaApi
--- frontend/Pro-Grama/src/auth/routes/authRoutes.jsx
+++ frontend/Pro-Grama/src/auth/routes/authRoutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
-import { Login, Register, Register2, Register3 } from '../pages'
+import { Login } from '../pages'
+import { MainRegister } from '../pages/MainRegister'
 
 
 export const AuthRoutes = () => {
@@ -8,9 +9,7 @@ export const AuthRoutes = () => {
     <>
     <Routes>
         <Route path="login" element={ <Login /> } />
-        <Route path="register" element={ <Register/> } />
-        <Route path="register2" element={ <Register2/> } />
-        <Route path="register3" element={ <Register3/> } />
+        <Route path="register" element={ <MainRegister/> } />
         <Route path='/*' element={ <Navigate to="/auth/login" /> } />
     </Routes>
     </>
