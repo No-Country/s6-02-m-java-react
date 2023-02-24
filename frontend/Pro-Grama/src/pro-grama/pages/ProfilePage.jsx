4d9c53bd7523e1c32026035642359bb0cee78109
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-02-24 11:15:52 -0300

    user info added to profile page

--- frontend/Pro-Grama/src/pro-grama/pages/ProfilePage.jsx
+++ frontend/Pro-Grama/src/pro-grama/pages/ProfilePage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
+import { BiEdit } from "react-icons/bi";
 
 export const ProfilePage = () => {
   const { token } = useSelector((state) => state.auth);
@@ -20,10 +21,20 @@ export const ProfilePage = () => {
 
   return (
     <>
-      <div>
-        <div>
-          <div>{user?.email}</div>
-          <div></div>
+      <div className="border border-dashed border-red-400">
+        <div className="flex justify-between w-fit">
+          <div className="border border-dashed border-red-400 flex">
+            <div className="rounded-full h-20 w-20 bg-gray-400" />
+            <div className="row text-3xl px-4">
+              {user?.name} {user?.lastname}
+              <div className="col border border-dashed border-red-400 text-lg">
+                {user?.email}
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-end">
+            <BiEdit className="text-4xl" />
+          </div>
         </div>
       </div>
     </>
