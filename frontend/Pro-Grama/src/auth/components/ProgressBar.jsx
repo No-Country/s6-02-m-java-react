23c0437672854a7a0ffdc300daddf39546ba1eaf
FranciscoSantos01 <franciscosantosaraoz2000@gmail.com>
2023-02-27 09:37:01 -0300

    update profile user and validations in form

--- frontend/Pro-Grama/src/auth/components/ProgressBar.jsx
+++ frontend/Pro-Grama/src/auth/components/ProgressBar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import './styles.css'
 
 export const ProgressBar = () => {
+  const{page}=useSelector(state=>state.auth)
   return (
     <div class="container1">
         <div class="progress-container">
         <div class="progress" id="progress"></div>
-        <div class="circle active bg-slate-700">1</div>
-        <div class="circle  bg-slate-700">2</div>
-        <div class="circle  bg-slate-500">3</div>
+        <div class={page === 1 ? "circle bg-Green" : "circle bg-GrayDark"}>1</div>
+        <div class={page === 2 ? "circle bg-Green" : "circle bg-GrayDark"}>2</div>
+        <div class={page === 3 ? "circle bg-Green" : "circle bg-GrayDark"}>3</div>
         </div>
    </div>
   )
--- frontend/Pro-Grama/src/pro-grama/pages/ProfilePage.jsx
+++ frontend/Pro-Grama/src/pro-grama/pages/ProfilePage.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import ProfileUserInfo from "../components/ProfileSections/ProfileUserInfo";
+import { UpdateUser } from "../components/ProfileSections/UpdateUser";
 
 export const ProfilePage = () => {
+  const[state,setState] = useState(false)
+  if(state === true){
+    return(
+      <UpdateUser />
+    )
+  }
   return (
     <>
       <div className="min-h-screen">
