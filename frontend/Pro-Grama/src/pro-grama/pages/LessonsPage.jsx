57cddd8015d23e5b8e152905badc5f9215deeaf5
FranciscoSantos01 <franciscosantosaraoz2000@gmail.com>
2023-03-01 20:53:04 -0300

    actualizando lesson course componente

--- frontend/Pro-Grama/src/pro-grama/pages/LessonsPage.jsx
+++ frontend/Pro-Grama/src/pro-grama/pages/LessonsPage.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { setCurso } from '../../store/lessons/lessonSlice'
 
 import LessonsCourses from '../components/Lessons/LessonsCourses'
 import Notes from '../components/Lessons/Notes/Notes'
 
 const LessonsPage = () => {
+  const dispatch = useDispatch();
+  const curso = JSON.parse(localStorage.getItem('lesson'));
+  useEffect(()=>{
+    if(curso){
+      dispatch(setCurso(curso))
+    }
+  },[])
   return (
     <>
       <LessonsCourses />
